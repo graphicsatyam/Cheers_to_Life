efb76cb102780a7971770f9eff216eab9cf18140
Satyam Maurya <122545925+graphicsatyam@users.noreply.github.com>
2024-06-06 22:42:50 +0530

    Update index.js

--- SERVER/index.js
+++ SERVER/index.js
@@ -18,7 +18,7 @@ import { UserRouter } from "./routes/user.js";
 // Middleware
 app.use(express.json()); // Parsing JSON bodies
 app.use(cors({
-    origin: /^https:\/\/cheers-to-life-frontend\.vercel\.app(:\d+)?(\/.*)?$/,
+    origin: ["https://cheers-to-life-frontend-ak6yrffkd-graphicsatyams-projects.vercel.app"],
     methods : ["POST", "GET"],
     credentials: true
 })); // Enable CORS with credentials
